4a4b3d5e92282521f19cf37d34b273aadf19dc71
Archit Jain <arcyjain2002@gmail.com>
2022-02-23 15:03:31 +0530

    started with backend socket

---
+++ src/backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("new connection" + socket.id);
+});
+
+httpServer.listen(3000);
